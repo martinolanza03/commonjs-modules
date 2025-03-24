792286879e842a017c094a66dbf447d38cab8780
martinolanza03 <lanzamartino3@gmail.com>
2025-03-24 15:27:12 +0100

    feat: create and export function hobbies to return array

---
+++ hobbies.js
@@ -0,0 +1,12 @@
+/* 
+    2 - hobbies.js
+1. Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire  un array di stringhe contenenti gli hobby.
+2. Esporta la funzione dal file.
+
+*/
+
+function hobbies(hobbyOne, hobbyTwo, hobbyThree) {
+    return [hobbyOne, hobbyTwo, hobbyThree];
+}
+
+module.export = hobbies;
