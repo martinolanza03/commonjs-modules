cda4512b6971c773965d23e378b93e483496d8b4
martinolanza03 <lanzamartino3@gmail.com>
2025-03-24 15:18:50 +0100

    feat: create a function to return object names

---
+++ names.js
@@ -0,0 +1,11 @@
+/* 
+    1 - names.js
+1. Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
+2. Esporta la funzione dal file.
+
+*/
+
+
+function names(firstName, lastName) {
+    return { firstName, lastName };
+}
